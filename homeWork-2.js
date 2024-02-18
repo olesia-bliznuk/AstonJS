e393d4bb1c388635329cc8c57e5b5ded99b58bbe
Olesya Blizniuk <olesyablizniuk@gmail.com>
2024-02-18 18:41:22 +0500

    ASTONJS-2 [feat]: add homework 2

---
+++ homeWork-2.js
@@ -0,0 +1,31 @@
+function getLength(val) {
+    return (val && val.length !== undefined) ? val.length : 0;
+}
+
+function compare(tree1, tree2) {
+    if (!tree1 && !tree2) return true;
+    if (!tree1 || !tree2) return false;
+
+    const queue1 = [tree1];
+    const queue2 = [tree2];
+
+    while (queue1.length && queue2.length) {
+
+        const currentNode1 = queue1.shift();
+        const currentNode2 = queue2.shift();
+
+        if (!currentNode1 || !currentNode2 || currentNode1.value !== currentNode2.value)
+            return false;
+
+        if (currentNode1.left)
+            queue1.push(currentNode1.left);
+        if (currentNode1.right)
+            queue1.push(currentNode1.right);
+        if (currentNode2.left)
+            queue2.push(currentNode2.left);
+        if (currentNode2.right)
+            queue2.push(currentNode2.right);
+    }
+
+    return queue1.length === queue2.length;
+}
