1c6d3d56f9e9481475c4eeb2fbeb82ce204c7f6d
Olesya Blizniuk <olesyablizniuk@gmail.com>
2024-02-29 11:21:12 +0500

    ASTONJS-5 [fix]: improve the algorithm for task 4

--- homeWork-5.js
+++ homeWork-5.js
@@ -14,7 +14,7 @@
 
 const Person = {
     name: 'Ann',
-    logInfo: function(){
+    logInfo: function () {
         console.log(this.name);
     }
 }
@@ -30,34 +30,34 @@ function Person1(name) {
     this.name = name;
 }
 
-Person1.prototype.logInfo = function() {
+Person1.prototype.logInfo = function () {
     console.log(this.name);
 };
 
-function AnotherPerson1(name){
+function AnotherPerson1(name) {
     Person1.call(this, name);
 }
 
-AnotherPerson1.prototype = Object.create(Person1.prototype); 
+AnotherPerson1.prototype = Object.create(Person1.prototype);
 AnotherPerson1.prototype.constructor = AnotherPerson1;
 
 const anotherPerson1 = new AnotherPerson1("Alice");
-anotherPerson1.logInfo(); 
+anotherPerson1.logInfo();
 
 //И спользованием класса
 
 class Person2 {
-    constructor(name){
+    constructor(name) {
         this.name = name;
     }
 
-    logInfo(){
+    logInfo() {
         console.log(this.name);
     }
 }
 
-class AnotherPerson2 extends Person2{
-    constructor (name){
+class AnotherPerson2 extends Person2 {
+    constructor(name) {
         super(name);
     }
 
@@ -69,39 +69,44 @@ anotherperson2.logInfo();
 //3. Создать класс SuperPerson c get и set для поля name и конструктором , сделать класс наследник от
 // класса SuperPerson.
 
-class SuperPerson{
-    constructor(name){
+class SuperPerson {
+    constructor(name) {
         this._name = name;
     }
-    get name(){
+    get name() {
         return this._name;
     }
-    set name(newName){
+    set name(newName) {
         this._name = newName;
     }
 }
 
-class PersonNew extends SuperPerson{
-    constructor(name){
+class PersonNew extends SuperPerson {
+    constructor(name) {
         super(name);
     }
 }
 
 // 4. Написать функцию которая вернет массив с первой парой чисел, сумма которых равна total
+
 const firstSum = (arr, total) => {
-    for (let i = 0; i < arr.length; i++){
-        for ( let j = i + 1; j < arr.length; j++){
-            if ( arr[i] + arr[j] === total)
-                return [arr[i], arr[j]]; 
+    const numMap = new Map(arr.map((num, index) => [num, index]));
+    for (let num of arr) {
+        const complement = total - num;
+        if (numMap.has(complement)) {
+            return [num, complement];
         }
     }
     return [];
-}
+};
 
 const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 const total = 13;
-console.log(firstSum(arr, total)); //result = [4, 9]
+console.log(firstSum(arr, total)); 
 
-//Так как функция содержит вложенный цикл, т.е внешний цикл проходится по каждому элементу массива 
-// arr (n итераций) и внутренний цикл также проходится по каждому элементу массива arr (еще n 
-// итераций), то сложность алгоритма будет O(n^2)
+//Сложность алгоритма составляет O(n). Cоздание numMap происходит за O(n), так как мы проходим 
+//по всем элементам массива один раз и затем в худшем случае мы снова проходим весь массив в 
+//цикле for за O(n). Получается O(2n), но множитель не влияет на сложность, поэтому его можно опустить
+
+
+
